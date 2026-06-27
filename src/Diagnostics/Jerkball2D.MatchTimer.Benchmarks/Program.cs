26ee48f95da52a12866346a68af3611edf073d61
KXnightGitHub <sharknagem@gmail.com>
2026-06-27 15:00:09 +0200

    ✨ chore: add dedicated Program entrypoint for BenchmarkDotNet suite

---
+++ src/Diagnostics/Jerkball2D.MatchTimer.Benchmarks/Program.cs
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MPL-2.0
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) 2026 kx-night
+
+using BenchmarkDotNet.Running;
+
+namespace Jerkball2DMatchTimer.Benchmarks;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+    }
+}
